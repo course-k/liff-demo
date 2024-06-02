0724c5ab29691c0c0e999052498781c86591df7f
practice_user <practice@mail.co.jp>
2024-06-02 22:24:14 +0900

    modify App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,29 @@ import liff from "@line/liff";
 import { useEffect, useState } from "react";
 
 function App() {
+  const [userName, setUserName] = useState("名無し");
   useEffect(() => {
     liff
       .init({ liffId: import.meta.env.VITE_LIFF_ID })
       .then(() => {
         console.log("init");
         console.log(liff);
+        liff
+          .getProfile()
+          .then((profile) => {
+            console.log("get profile");
+            console.log(liff);
+            console.log(profile);
+            setUserName(profile.displayName);
+          })
+          .catch(() => {
+            console.log("get profile failed");
+          });
       })
       .catch(() => {
         console.log("init failed");
       });
   }, []);
-  const [userName, setUserName] = useState("名無し");
-  liff
-    .getProfile()
-    .then((profile) => {
-      console.log("get profile");
-      console.log(liff);
-      console.log(profile);
-      setUserName(profile.displayName);
-    })
-    .catch(() => {
-      console.log("get profile failed");
-    });
 
   return (
     <>
